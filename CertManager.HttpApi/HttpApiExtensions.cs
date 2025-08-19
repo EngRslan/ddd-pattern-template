c42c33bbd5dc1337c2cd9d1250f567cda53dee67
Mohamed Rslan <eng.m.rslan@hotmail.com>
2025-08-19 21:41:40 +0300

    refactor project naming

--- CertManager.HttpApi/HttpApiExtensions.cs
+++ CertManager.HttpApi/HttpApiExtensions.cs
@@ -2,9 +2,11 @@ using System.Reflection;
 using CertManager.Domain.Shared.DependencyInjection;
 using CertManager.HttpApi.Middleware;
 using FastEndpoints;
+using FastEndpoints.Swagger;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Scalar.AspNetCore;
 
 namespace CertManager.HttpApi;
 
@@ -16,6 +18,39 @@ public static class HttpApiExtensions
     {
         services.AddFastEndpoints();
         services.AddHttpContextAccessor();
+        
+        // Configure OpenAPI/Swagger document generation
+        services.SwaggerDocument(o =>
+        {
+            o.DocumentSettings = s =>
+            {
+                s.Title = "CertManager API";
+                s.Version = "v1";
+                s.Description = "Domain-Driven Design Template API Documentation";
+                s.DocumentName = "v1";
+            };
+            
+            o.EnableJWTBearerAuth = false; // Set to true if using JWT
+            o.TagDescriptions = t =>
+            {
+                t["Home"] = "Server information endpoints";
+            };
+            
+            o.AutoTagPathSegmentIndex = 0; // Auto-tag by first path segment after prefix
+        });
+        
+        // Add additional API versions if needed
+        services.SwaggerDocument(o =>
+        {
+            o.DocumentSettings = s =>
+            {
+                s.Title = "CertManager API";
+                s.Version = "v2";
+                s.DocumentName = "v2";
+            };
+            o.MaxEndpointVersion = 2;
+            o.AutoTagPathSegmentIndex = 0;
+        });
         
         // Configure CORS
         var allowedOrigins = configuration.GetSection("AllowedOrigins").Get<string[]>() ?? ["*"];
@@ -57,7 +92,16 @@ public static class HttpApiExtensions
             {
                 conf.Endpoints.RoutePrefix = "api";
             });
+            e.MapScalarApiReference("/docs", r =>
+            {
+                r.WithTitle("CertManager API Documentation");
+                r.WithTheme(ScalarTheme.Moon);
+                r.WithOpenApiRoutePattern("swagger/{documentName}/swagger.json");
+            });
         });
+        
+        // OpenAPI document generation endpoints
+        app.UseSwaggerGen(); 
         return app;
     }
 }
