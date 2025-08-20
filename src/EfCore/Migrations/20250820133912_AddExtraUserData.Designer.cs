f73553a4ff551bdacf8e0e2189430e1660d48063
Muhammad Raslan <m.raslan@klues.ai>
2025-08-20 16:40:49 +0300

    fix colors and add extra use migration

--- src/EfCore/Migrations/20250820133912_AddExtraUserData.Designer.cs
+++ src/EfCore/Migrations/20250820133912_AddExtraUserData.Designer.cs
@@ -4,6 +4,7 @@ using CertManager.EfCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CertManager.EfCore.Migrations
 {
     [DbContext(typeof(IdentityDataContext))]
-    partial class IdentityDataContextModelSnapshot : ModelSnapshot
+    [Migration("20250820133912_AddExtraUserData")]
+    partial class AddExtraUserData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
